8b58b52152a4e5898a52df944a7528494cbde2d2
apurav gaur <apurav gaur>
2016-11-03 03:40:57 +0530

    add project

---
+++ password-strength-checker.py
@@ -0,0 +1,16 @@
+########################################################
+#        Program to check password strength from       #
+#                                                      #
+#                                                      #
+#                 Author - Apurav Gaur                 #
+#                                                      #
+#                  dated- 2 Nov 2016                   #
+#                						               #
+########################################################
+
+password = raw_input('Enter the password ')
+special = '!@#$%^&*()?'
+if len(password) >= 8 and not password.islower() and not password.isupper() and not password.isalpha() and not password.isdigit() and any((c in special) for c in password):
+    print 'password is strong'
+else:
+    print 'Weak Password'
