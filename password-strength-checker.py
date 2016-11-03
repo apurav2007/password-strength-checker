90b9b5946d52a510002a42429c861bbcd21861ac
apurav gaur <apurav gaur>
2016-11-04 00:47:32 +0530

    update new code

--- password-strength-checker.py
+++ password-strength-checker.py
@@ -8,9 +8,24 @@
 #                						               #
 ########################################################
 
-password = raw_input('Enter the password ')
-special = '!@#$%^&*()?'
-if len(password) >= 8 and not password.islower() and not password.isupper() and not password.isalpha() and not password.isdigit() and any((c in special) for c in password):
-    print 'password is strong'
-else:
-    print 'Weak Password'
+import re
+
+while True:
+  	password = raw_input('Password: ... ')
+
+    	if 6 <= len(password) <= 12:
+    		break
+    		print 'The password must be between 6 and 12 characters.\n'
+
+password_scores = {0:'Horrible', 1:'Weak', 2:'Medium', 3:'Strong'}
+password_strength = dict.fromkeys(['has_upper', 'has_lower', 'has_num'], False)
+if re.search(r'[A-Z]', password):
+	password_strength['has_upper'] = True
+if re.search(r'[a-z]', password):
+	password_strength['has_lower'] = True
+if re.search(r'[0-9]', password):
+	password_strength['has_num'] = True
+
+score = len([b for b in password_strength.values() if b])
+
+print 'Password is %s' % password_scores[score]
